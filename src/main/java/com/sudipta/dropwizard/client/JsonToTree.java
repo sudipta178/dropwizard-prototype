6b06eab548f60c0fdc6edae1d3806092fa30524c
sudichat <sudichat@cisco.com>
2018-09-24 03:21:14 -0700

    Adding unique node set

--- src/main/java/com/sudipta/dropwizard/client/JsonToTree.java
+++ src/main/java/com/sudipta/dropwizard/client/JsonToTree.java
@@ -3,27 +3,28 @@ package com.sudipta.dropwizard.client;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.Stack;
 
 public class JsonToTree {
     //Create an ObjectMapper instance
     private static  ObjectMapper mapper = new ObjectMapper();
-    private Stack<JsonNode> nodeStack = new Stack<>();
-    private Stack<String> pathStack = new Stack<>();
+    private Set<String> nodeSet = new HashSet<>();
 
     public Set<String> getJsonTreePaths() {
         Set<String> pathSet = new HashSet<>();
-        String [] paths = new String[100];
+        List<String> paths = new ArrayList<>();
         try {
             //Create tree from JSON
             InputStream jsonInput = JsonToTree.class.getClassLoader().getResourceAsStream("assets/json/correctedJson.json");
             JsonNode rootNode = mapper.readTree(jsonInput);
             System.out.println("Input JSON Read");
 
-            parseNode(rootNode, paths, 0, pathSet);
+            parseNode(rootNode, paths, pathSet);
             System.out.println("JSON Tree Parse Done");
 
             System.out.println("Total Paths: " + pathSet.size());
@@ -36,7 +37,7 @@ public class JsonToTree {
         return pathSet;
     }
 
-    private void parseNode(JsonNode rootNode, String [] paths, int pathLen, Set<String> pathSet) {
+    private void parseNode(JsonNode rootNode, List<String> paths, Set<String> pathSet) {
         if (null != rootNode) {
             /* append this node to the path array */
             String value =  rootNode.findValue("name").textValue();
@@ -44,36 +45,73 @@ public class JsonToTree {
                 value = String.valueOf(rootNode.findValue("id").intValue());
             }
             if (null != value && !"".equals(value)) {
-                paths[pathLen] = value.trim();
-                pathLen++;
+                paths.add(value.trim());
             }
+            nodeSet.add(value);
 
             /* it's a leaf, so print the path that led to here  */
             JsonNode childNodes = rootNode.findValue("children");
             if (null == childNodes) {
-                printPath(paths, pathLen, pathSet);
+                printPath(paths, pathSet);
             }
             else {
+                int size = 0;
                 Iterator<JsonNode> nodeIterator = childNodes.elements();
                 while (nodeIterator.hasNext()) {
+                    nodeIterator.next();
+                    size++;
+                }
+                if (size > 1) {
+                    printPath(paths, pathSet, false);
+                }
+                nodeIterator = childNodes.elements();
+                while (nodeIterator.hasNext()) {
                     JsonNode childNode = nodeIterator.next();
-                    parseNode(childNode, paths, pathLen, pathSet);
+                    parseNode(childNode, paths, pathSet);
                 }
             }
         }
     }
 
-    private void printPath(String [] paths, int pathLen, Set<String> pathSet) {
+    private void printPath(List<String> paths, Set<String> pathSet) {
         StringBuilder pathBuilder = new StringBuilder();
-        for(int i = pathLen - 1; i >= 0; i--) {
-            pathBuilder.append(paths[i]);
-            if (i != 0) {
-                pathBuilder.append(" -> ");
-            }
+        Stack<String> patStack = new Stack<>();
+        int index = 0;
+        Iterator<String> pathIterator = paths.iterator();
+        while (pathIterator.hasNext()) {
+            index++;
+            patStack.add(pathIterator.next());
+            pathIterator.remove();
+        }
+        while (!patStack.empty()) {
+            pathBuilder.append(patStack.pop()).append(" -> ");
         }
         if (!"".equals(pathBuilder.toString())) {
-            pathSet.add(pathBuilder.toString());
+            pathSet.add(pathBuilder.toString().substring(0, pathBuilder.toString().lastIndexOf(" -> ")));
         }
     }
 
+    private void printPath(List<String> paths, Set<String> pathSet, boolean deleteNode) {
+        StringBuilder pathBuilder = new StringBuilder();
+        Stack<String> patStack = new Stack<>();
+        int index = 0;
+        Iterator<String> pathIterator = paths.iterator();
+        while (pathIterator.hasNext()) {
+            index++;
+            patStack.add(pathIterator.next());
+            if (index != paths.size()) {
+                pathIterator.remove();
+            }
+        }
+        while (!patStack.empty()) {
+            pathBuilder.append(patStack.pop()).append(" -> ");
+        }
+        if (!"".equals(pathBuilder.toString())) {
+            pathSet.add(pathBuilder.toString().substring(0, pathBuilder.toString().lastIndexOf(" -> ")));
+        }
+    }
+
+    public Set<String> getNodeSet() {
+        return nodeSet;
+    }
 }
--- src/test/java/com/sudipta/dropwizard/client/JsonToTreeTest.java
+++ src/test/java/com/sudipta/dropwizard/client/JsonToTreeTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,9 +29,11 @@ public class JsonToTreeTest {
     public void init() {
         mockPathSet = new HashSet<>(4);
         mockPathSet.add("start -> fetch_submission_info -> review_request -> end");
-        mockPathSet.add("select_ez_best_tier1_sanity -> skip_ez_best_tier1_sanity -> check_ez_best_tier1_sanity -> cleanup -> end");
-        mockPathSet.add("check_imge -> build_nxos -> prepare_for_test -> select_ez_best_tier1_sanity -> submit_ez_best_tier1_sanity -> check_ez_best_tier1_sanity -> cleanup -> end");
-        mockPathSet.add("fetch_submission_info -> select_test -> check_free_space -> check_imge -> skip_build_nxox -> prepare_for_test -> select_ez_best_tier1_sanity -> submit_ez_best_tier1_sanity -> check_ez_best_tier1_sanity -> cleanup -> end");
+        mockPathSet.add("fetch_submission_info -> select_test -> check_free_space -> check_imge -> skip_build_nxox -> prepare_for_test");
+        mockPathSet.add("check_imge -> build_nxos -> prepare_for_test");
+        mockPathSet.add("prepare_for_test -> select_ez_best_tier1_sanity -> submit_ez_best_tier1_sanity -> check_ez_best_tier1_sanity");
+        mockPathSet.add("select_ez_best_tier1_sanity -> skip_ez_best_tier1_sanity -> check_ez_best_tier1_sanity");
+        mockPathSet.add("check_ez_best_tier1_sanity -> cleanup -> end");
     }
 
 
@@ -39,14 +44,30 @@ public class JsonToTreeTest {
 
         assertNotNull(pathSet);
         assertFalse(pathSet.isEmpty());
-        assertEquals(4, pathSet.size());
+        assertEquals(6, pathSet.size());
 
-        assertEquals(4, mockPathSet.size());
+        assertEquals(6, mockPathSet.size());
         for (String path : pathSet) {
             assertNotNull(path);
             assertTrue(mockPathSet.remove(path));
         }
         assertEquals(0, mockPathSet.size());
+    }
+
+    @DisplayName("testGetUniqueNodes test successful")
+    @Test
+    public void testGetNodeSet() {
+        mockJsonToTree.getJsonTreePaths();
+        Set<String> nodeSet = mockJsonToTree.getNodeSet();
+
+        assertNotNull(nodeSet);
+        assertEquals(15, nodeSet.size());
+
+        List<String> list = new ArrayList<>(nodeSet);
+        Collections.sort(list);
+        for (String nodeName : list) {
+            assertNotNull(nodeName);
+        }
     }
 
 }
